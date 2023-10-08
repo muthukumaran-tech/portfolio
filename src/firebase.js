1ab7a498858e912efa5d9da41f27c5b3634efa73
muthu <muthukumaran.perumal@cirantait.com>
2023-10-08 20:02:07 +0530

    build muthukumaran

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD_hesa_svPU66mfs4B_TQ13W4GaiIWJmU",
-  authDomain: "mk17phoenix.firebaseapp.com",
+  authDomain: "mk-portfolio-f7ed2.firebaseapp.com",
   databaseURL: "https://mk-portfolio-f7ed2-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "mk-portfolio-f7ed2",
   storageBucket: "mk-portfolio-f7ed2.appspot.com",
   messagingSenderId: "165224919305",
-  appId: "1:165224919305:web:5665b3468bacdf848563d8",
-  measurementId: "G-9R668L9R8T"
+  appId: "1:165224919305:web:00a4dd8c1cf97fad8563d8",
+  measurementId: "G-GWKXH0EFRC"
 };
 
 // Initialize Firebase
